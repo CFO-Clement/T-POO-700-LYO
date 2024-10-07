95d0bcc47c497addc0de138763e6fad5221e2f6c
Clement FERRERO <clement.ferrero@yahoo.fr>
2024-10-07 15:10:30 +0200

    Initial Commit

--- lib/time_manager_web/controllers/task_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule TimeManagerWeb.TaskController do
-    use TimeManagerWeb, :controller
-  
-    def task(conn, _params) do
-      # The home page is often custom made,
-      # so skip the default app layout.
-      render(conn, :task, layout: false)
-    end
-  end
--- lib/time_manager_web/controllers/user_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule TimeManagerWeb.UserController do
-    use TimeManagerWeb, :controller
-  
-    def user(conn, _params) do
-      # The home page is often custom made,
-      # so skip the default app layout.
-      render(conn, :user, layout: false)
-    end
-  end
